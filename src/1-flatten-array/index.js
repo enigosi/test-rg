f33b2839a39b6710fd5acd4b21572bd3ea4ae624
Wojciech Romański <enigosi@gmail.com>
2018-01-07 14:40:40 +0100

    add flattenArray function

--- src/1-flatten-array/index.js
+++ src/1-flatten-array/index.js
@@ -1,5 +1,9 @@
 
+module.exports = function flattenArray(arrayToFlatten) {
+  if (!Array.isArray(arrayToFlatten)) return null;
 
-module.exports = function flattenArray() {
-
+  return arrayToFlatten.reduce((prev, curr) => {
+    const flattenedCurr = Array.isArray(curr) ? flattenArray(curr) : [curr];
+    return prev.concat(flattenedCurr);
+  }, []);
 };
